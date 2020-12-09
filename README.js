69660155075b58c78f837bff9c456d942c4fe7a5
alilakhdim <75641414+alilakhdim@users.noreply.github.com>
2020-12-09 23:02:40 +0100

    Update README.js

--- README.js
+++ README.js
@@ -2,7 +2,7 @@
 $(document).ready(function()
 {
    if($("#blogID").attr("blogID")!="7754989255366973915" ) 
-   if($("#blogID").attr("blogID")!="2177468069970648534" ) 
+   if($("#blogID").attr("blogID")!="8344065380968680925" ) 
    if($("#blogID").attr("blogID")!="4579856093246931692" ) 
 {
    window.location.href="https://www.tdeex.com";
